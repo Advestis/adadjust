d8ab548cd4381e57543289d5e80cce95d20286e4
pcotte <pcotte@advestis.com>
2021-12-07 12:46:16 +0100

    chg: docstring

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
-# AdFit
+---
+permalink: /docs/index.html
+---
 
-Implement the notion of fittable function
+**The official documentation is available at https://advestis.github.io/adadjust/**
+
+# AdAdjust
+
 
 ## Installation
 
@@ -26,20 +31,23 @@ x = np.linspace(xstart, xend, nsamples)
 y = a * x ** 2 + b + np.random.normal(0, noise, nsamples)
 
 
-def linfunc(xx, aa, bb):
-    return xx * aa + bb
+def linfunc(xx, p):
+    return xx * p[0] + p[1]
 
 
-def square(xx, aa, bb):
-    return xx ** 2 * aa + bb
+def square(xx, p):
+    return xx ** 2 * p[0] + p[1]
 
 
 func = Function(linfunc, "$a \\times p[0] + p[1]$")
 func2 = Function(square, "$a^2 \\times p[0] + p[1]$")
-params = func.fit(x, y, [0, 0])[0]
+
+params = func.fit(x, y, np.array([0, 0]))[0]
 rr = func.compute_rsquared(x, y, params)
-params2 = func2.fit(x, y, [0, 0])[0]
+
+params2 = func2.fit(x, y, np.array([0, 0]))[0]
 rr2 = func2.compute_rsquared(x, y, params2)
+
 table = Function.make_table(
     [func, func2], [params, params2], [rr, rr2], caption="Linear and Square fit", path_output="table.pdf"
 )
@@ -47,3 +55,10 @@ table.compile()
 Function.plot(x, [func, func2], [params, params2], y=y, rsquared=[rr, rr2])
 plt.gcf().savefig("plot.pdf")
 ```
+
+**NOTE** : to have pretty gaphs, put the line `plt.rcParams.update({"text.usetex": True})` just after you imported adadjust.
+This requiers that you have TexLive full installed on your computer.
+
+The result will be :
+
+![Alt text](tests/data/plot.png)
--- tests/test_adadjust.py
+++ tests/test_adadjust.py
@@ -2,6 +2,7 @@ from adadjust import Function
 import numpy as np
 import matplotlib.pyplot as plt
 
+plt.rcParams.update({"text.usetex": True})
 
 nsamples = 1000
 a = 0.3
@@ -22,43 +23,44 @@ y = a * x ** 2 + b + np.random.normal(0, noise, nsamples)
 # ])
 
 
-def linfunc(xx, aa, bb):
-    return xx * aa + bb
+def linfunc(xx, p, n=1):
+    return n * (xx * p[0] + p[1])
 
 
 # def linfunc2d(xx, aa1, bb1, aa2, bb2):
 #     return xx[0] * aa1 + bb1 + xx[1] * aa2 + bb2
 
 
-def square(xx, aa, bb):
-    return xx ** 2 * aa + bb
+def square(xx, p):
+    return 2 * (xx ** 2 * p[0] + p[1])
 
 
 def test_fit():
-    func = Function(linfunc, "$a \\times p[0] + p[1]$")
-    func2 = Function(square, "$a^2 \\times p[0] + p[1]$")
-    params = func.fit(x, y, [0, 0])[0]
-    rr = func.compute_rsquared(x, y, params)
-    params2 = func2.fit(x, y, [0, 0])[0]
-    rr2 = func2.compute_rsquared(x, y, params2)
+    func = Function(linfunc, "$n \\times (p[0] \\times x + p[1]$)")
+    func2 = Function(square, "$p[0] \\times x^2 + p[1]$")
+    params = func.fit(x, y, np.array([0, 0]), args=(2,))[0]
+    rr = round(func.compute_rsquared(x, y, params, 2), 3)
+    params2 = func2.fit(x, y, np.array([0, 0]))[0]
+    rr2 = round(func2.compute_rsquared(x, y, params2), 3)
     table = Function.make_table(
         [func, func2], [params, params2], [rr, rr2], caption="Linear and Square fit", path_output="tests/data/table.pdf"
     )
     table.compile()
-    Function.plot(x, [func, func2], [params, params2], y=y, rsquared=[rr, rr2])
-    plt.gcf().savefig("tests/data/plot.pdf")
+    Function.plot(x, [func, func2], [params, params2], argss=[(2,), ()], y=y, rsquared=[rr, rr2])
+    plt.gcf().savefig("tests/data/plot.png")
 
 
-def test_fit_2d():
-    func = Function(linfunc, "$a \\times p[0] + p[1]$")
-    func2 = Function(square, "$a^2 \\times p[0] + p[1]$")
-    params = func.fit(x, y, [0, 0])[0]
-    rr = func.compute_rsquared(x, y, params)
-    params2 = func2.fit(x, y, [0, 0])[0]
-    rr2 = func2.compute_rsquared(x, y, params2)
-    table = Function.make_table(
-        [func, func2], [params, params2], [rr, rr2], caption="Linear and Square fit", path_output="tests/data/table.pdf"
-    )
-    table.compile()
-    Function.plot(x, [func, func2], [params, params2], y=y, rsquared=[rr, rr2])
-    plt.gcf().savefig("tests/data/plot.pdf")
+#
+# def test_fit_2d():
+#     func = Function(linfunc, "$a \\times p[0] + p[1]$")
+#     func2 = Function(square, "$a^2 \\times p[0] + p[1]$")
+#     params = func.fit(x, y, np.array([0, 0]))[0]
+#     rr = func.compute_rsquared(x, y, params)
+#     params2 = func2.fit(x, y, np.array([0, 0]))[0]
+#     rr2 = func2.compute_rsquared(x, y, params2)
+#     table = Function.make_table(
+#         [func, func2], [params, params2], [rr, rr2], caption="Linear and Square fit", path_output="tests/data/table.pdf"
+#     )
+#     table.compile()
+#     Function.plot(x, [func, func2], [params, params2], y=y, rsquared=[rr, rr2])
+#     plt.gcf().savefig("tests/data/plot.pdf")
