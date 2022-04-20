01cf284a72da316e4c7c427dfc92e469c9cb4013
pcotte <pcotte@advestis.com>
2022-04-20 13:39:49 +0200

    fix: setup.py

--- adadjust/__init__.py
+++ adadjust/__init__.py
@@ -1,6 +1,4 @@
 from .functions import Function
 
-try:
-    from ._version import __version__
-except ImportError:
-    pass
+from . import _version
+__version__ = _version.get_versions()['version']
